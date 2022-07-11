7d84e853686209dc2065377eb0f56305e7f29cfc
sahiljain2497 <sahiljainpkl199724@gmail.com>
2022-07-11 14:47:22 +0530

    fix attendance scan

--- src/app/modules/common/update-attendance/update-attendance.component.ts
+++ src/app/modules/common/update-attendance/update-attendance.component.ts
@@ -1,5 +1,11 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/member-ordering */
 import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+    MatDialog,
+    MatDialogRef,
+    MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { FuseConfirmationService } from '@fuse/services/confirmation';
@@ -22,35 +28,34 @@ export class UpdateAttendanceComponent implements OnInit {
     lastData: AttendanceStatusObjectI;
     workingHours = '0';
     employeeData: boolean = false;
-    attendanceDetails;
+    employeeDetails: any;
+    attendanceDetails = [];
     constructor(
         private attendanceService: AttendanceService,
         private snackBar: MatSnackBar,
         private _fuseConfirmationService: FuseConfirmationService,
         @Inject(MAT_DIALOG_DATA) private data: { employeeId: string },
-        private dialog: MatDialog
+        private dialogRef: MatDialogRef<UpdateAttendanceComponent>
     ) {}
 
     ngOnInit(): void {
         this.employeeId = this.data.employeeId;
         this.getLastStatus();
     }
+
     getLastStatus() {
         this.attendanceService.getLastAttendance(this.employeeId).subscribe(
             (res: any) => {
-                this.attendanceDetails = res.data.attendance.attendanceStatus;
-                console.log(this.attendanceDetails);
+                this.lastData = res.data.attendance;
+                this.employeeDetails = res.data.employee;
+                console.log('this.lastData', this.lastData);
                 this.employeeData = true;
-                if (
-                    (res as AttendanceNoDataResponse).data?.lastStatus
-                        ?.lastStatus
-                ) {
+                if (!res.data.attendance) {
                     this.snackBar.open('No Record Found For Today', 'Close', {
                         duration: 3000,
                         panelClass: ['alert-red'],
                     });
                 } else {
-                    this.lastData = (res as AttendanceResponse).data.lastStatus;
                     this.workingHours =
                         this.attendanceService.calculateWorkingTime(
                             this.lastData.attendanceStatus
@@ -59,9 +64,18 @@ export class UpdateAttendanceComponent implements OnInit {
             },
             (err) => {
                 console.log(err);
+                this.snackBar.open(
+                    'Unable to get attendance details',
+                    'Close',
+                    {
+                        duration: 3000,
+                        panelClass: ['alert-red'],
+                    }
+                );
             }
         );
     }
+
     saveAttendance(status: AttendanceStatus) {
         console.log(this.employeeId);
         return this.attendanceService
@@ -71,6 +85,7 @@ export class UpdateAttendanceComponent implements OnInit {
                     this.snackBar.open('Status Updated!', 'Close', {
                         duration: 3000,
                     });
+                    this.dialogRef.close();
                 },
                 (err) => {
                     this._fuseConfirmationService.open({
@@ -89,13 +104,14 @@ export class UpdateAttendanceComponent implements OnInit {
                 }
             );
     }
+
     get lastStatus() {
         if (!this.lastData) {
             return null;
         }
         return this.lastData.attendanceStatus[
             this.lastData.attendanceStatus.length - 1
-        ].status;
+        ];
     }
 
     get showWorkingButton() {
@@ -106,14 +122,22 @@ export class UpdateAttendanceComponent implements OnInit {
         return [
             AttendanceStatus.NOT_WORKING,
             AttendanceStatus.ON_BREAK,
-        ].includes(this.lastStatus);
+        ].includes(this.lastStatus.status);
     }
 
     get showBreakButton() {
         if (!this.lastData) {
             return false;
         }
+        return [AttendanceStatus.WORKING].includes(this.lastStatus.status);
+    }
 
-        return [AttendanceStatus.WORKING].includes(this.lastStatus);
+    get showDoneButton() {
+        if (!this.lastData) {
+            return false;
+        }
+        return [AttendanceStatus.WORKING, AttendanceStatus.ON_BREAK].includes(
+            this.lastStatus.status
+        );
     }
 }
